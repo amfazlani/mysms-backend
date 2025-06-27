03e6bd10d277d9870a6df7cbac2f86e447cd4739
Amaar Fazlani <fazlaniamaar@gmail.com>
2025-06-27 02:36:10 -0500

    Style message-from, message-list, and add navbar

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { MessageListComponent } from './components/message-list/message-list.component';
 import { MessageFormComponent } from './components/message-form/message-form.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 import { HttpClientModule } from '@angular/common/http'; // ✅ add this
 import { importProvidersFrom } from '@angular/core';
 
@@ -9,6 +10,7 @@ import { importProvidersFrom } from '@angular/core';
   templateUrl: './app.component.html',
   standalone: true,
   imports: [
+    NavbarComponent,
     MessageListComponent,
     MessageFormComponent,
   ],
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,10 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}", // 👈 Important: allows Tailwind to scan your components
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
