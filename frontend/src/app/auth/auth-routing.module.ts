64dd2e982772c005a5847c3ff22da5f7e9eb6b87
Amaar Fazlani <fazlaniamaar@gmail.com>
2025-06-28 07:14:58 -0500

    Modify root path to redirect to login

--- frontend/src/app/auth/auth-routing.module.ts
+++ frontend/src/app/auth/auth-routing.module.ts
@@ -4,6 +4,7 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 
 export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'login' },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
 ];
