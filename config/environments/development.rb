647915e2ab98934b3e57b4e5d86e4f5552f861d0
Amaar Fazlani <fazlaniamaar@gmail.com>
2025-06-29 02:02:57 -0500

    Add frontend url to action cable allowed request origins

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,6 +52,7 @@ Rails.application.configure do
 
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
+  config.hosts.clear
 
 
   # Raises error for missing translations.
@@ -62,4 +63,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.action_cable.allowed_request_origins = [ENV['CLIENT_URL']]
 end
